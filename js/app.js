1c5b856d875bae7ec95f1b7e25110ce87f3dcedd
stefanom13 <stefanomancuso13@gmail.com>
2022-02-18 09:55:12 +0100

    esercizio completo

--- js/app.js
+++ js/app.js
@@ -30,8 +30,37 @@ const team = [
       image:'barbara-ramos-graphic-designer.jpg',
     },
   ];
+
+  // selezionare class presente in html che contiene la card
+  const teamContaier = document.querySelector('.team-container')
+
+  // usare la card esistente  e per ogni elemento creare nuova card ed inserirla nel dom
   
-  
+for (let i = 0; i < team.length; i++) {
+  // vado a riprendere gli elementi presenti nell array team
+  const teamMember = team [i];
+  const name = teamMember.name
+  const role = teamMember.role
+  const image = teamMember.image
+
+  const htmlCard = `
+    <div class="team-card">
+     <div class="card-image">
+       <img
+         src="img/${ image }"
+         alt="${ name }"
+       />
+     </div>
+     <div class="card-text">
+       <h3>${ name }</h3>
+       <p>${ role }</p>
+     </div>
+   </div>`;
+
+   teamContaier.innerHTML += htmlCard;
+  //  inserisco le img nel team-conteiner
+
+}
   
   
   
